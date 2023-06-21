d7a0c49568c6626d6c9126cdf6a425b16053a6c2
Daelen Wall <daelenwall1@gmail.com>
2023-06-20 23:25:05 -0400

    about css work in progress but pretty much done

--- src/components/About.js
+++ src/components/About.js
@@ -1,19 +1,33 @@
-import React from 'react';
-// Here we are importing a CSS file as a dependency
+import React, { useState } from 'react';
 import '../styles/About.css';
 
 function About() {
+  const [isParagraphVisible, setIsParagraphVisible] = useState(false);
+
+  const toggleVisibility = () => {
+    setIsParagraphVisible(!isParagraphVisible);
+  };
+
   return (
-    <section class="about">
-        <div class="main">
+    <section className="about">
+      <div className="main">
         <section>
-            <div class="about-text">
-                <h1 id="about">About Me</h1>
-                <p> My name is Daelen Wall and I am a 19 year-old web developer from Connecticut.  I started coding in 2022 at UConn.  I specialize in HTML and CSS and can provide clean code and impeccable web design.</p>
-            </div>
+          <div className="about-text">
+            <h1 id="about">
+              About Me{' '}
+              <button onClick={toggleVisibility}>
+                {isParagraphVisible ? '▲' : '▼'}
+              </button>
+            </h1>
+            {isParagraphVisible && (
+              <p>
+                My name is Daelen Wall and I am a 19-year-old web developer from Connecticut. I started coding in 2022 at UConn. I specialize in HTML and CSS and can provide clean code and impeccable web design.
+              </p>
+            )}
+          </div>
+        </section>
+      </div>
     </section>
-    </div>
-  </section>
   );
 }
 
